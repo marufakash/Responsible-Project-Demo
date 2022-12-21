47d1f5c5cdf607c3f0470f22ba9805da7b4b0b56
maruf-akash <marufakash392@gmail.com>
2022-12-21 19:43:34 +0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,10 @@
+
+/* Open the sidenav */
+function openNav() {
+    document.querySelector("nav").style.width = "50%";
+  }
+  
+  /* Close/hide the sidenav */
+  function closeNav() {
+    document.querySelector("nav").style.width = "0";
+  }
